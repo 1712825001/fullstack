17b38d5fb7f0dd711563223f2169cfae929a7546
lining <1054057238@qq.com>
2019-03-27 11:51:45 +0800

    dwad

--- 【27】Python面向对象(一)/03 实例变量和类变量及实例属性访问顺序/code.py
+++ 【27】Python面向对象(一)/03 实例变量和类变量及实例属性访问顺序/code.py
@@ -14,12 +14,75 @@
 # jerry.showage()
 
 
-class MyClass:
-    def __init__(self):
-        print('self in init = {}'.format(id(self)))
+# class MyClass:
+#     def __init__(self):
+#         print('self in init = {}'.format(id(self)))
+#
+# c = MyClass()
+# print('c = {}'.format(id(c)))
+#
+# d = MyClass()
+# print('d = {}'.format(id(d)))
 
-c = MyClass()
-print('c = {}'.format(id(c)))
+# class Person:
+#     age = 3
+#     def __init__(self, name):
+#         self.name1 = name
+#
+#     def showname(self):
+#         print(self.name1)
+#
+# tom = Person('Tom')
+# jerry = Person('Jerry')
+#
+# print(tom.name1, tom.age)
+# print(jerry.name1, jerry.age)
+#
+# Person.age = 30
+# print(Person.age, tom.age, jerry.age)
+#
+# tom.name1 = 'tommy'
+# print(jerry.name1)
+# print(tom.name1)
 
-d = MyClass()
-print('d = {}'.format(id(d)))
+# class Person:
+#     age = 3
+#
+#     def __init__(self, name):
+#         self.name = name
+#
+# print('-----class-----')
+# print(Person.__class__)
+# print(sorted(Person.__dict__.items()), end='\n\n')
+#
+# tom = Person('Tom')
+# print('-----instance Tom-----')
+# print(tom.__class__)
+# print(sorted(tom.__dict__.items()), end='\n\n')
+
+class Person:
+    age = 3
+    height = 170
+
+    def __init__(self, name, age=18):
+        self.name = name
+        self.age = age
+
+tom = Person('Tom')
+jerry = Person('Jerry', 20)
+
+Person.age = 30
+print(Person.age, tom.age, jerry.age)
+print(Person.height, tom.height, jerry.height)
+
+jerry.height = 175
+print(Person.height, tom.height, jerry.height)
+
+tom.height += 10
+print(Person.height, tom.height, jerry.height)
+
+Person.height += 15
+print(Person.height, tom.height, jerry.height)
+
+Person.weight = 70
+print(Person.weight, tom.weight, jerry.weight)
