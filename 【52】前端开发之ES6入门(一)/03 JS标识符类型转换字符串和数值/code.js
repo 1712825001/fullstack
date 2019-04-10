df8a5bac556349a1302d02fbfddf6e186a35080d
lining <1054057238@qq.com>
2019-04-10 15:04:59 +0800

    8341

--- 【52】前端开发之ES6入门(一)/03 JS标识符类型转换字符串和数值/code.js
+++ 【52】前端开发之ES6入门(一)/03 JS标识符类型转换字符串和数值/code.js
@@ -48,4 +48,43 @@ console.log(undefined + false, typeof(a))
 console.log(null & true, typeof(a))
 console.log(undefined & true, typeof(a))
 */
+ // and or
+ /*
+ console.log(a = null && true, typeof(a))
+ console.log(a = false && null, typeof(a))
+ console.log(a = false && 'magedu', typeof(a))
+ console.log(a = true && 'magedu', typeof(a))
+ console.log(a = true && '', typeof(a))
+*/
 
+// 字符串
+/*
+var a = 100, b = 200;
+s = `${a} : ${b}`
+console.log(s)
+*/
+
+
+// 字符串
+let school = 'magedu'
+console.log(school.charAt(2))
+console.log(school[2])
+console.log(school.toUpperCase())
+console.log(school.concat('.com'))
+console.log(school.slice(3))
+console.log(school.slice(3,5))
+console.log(school.slice(-2.-1))
+console.log(school.slice(-2))
+
+let url = 'www.magedu.com'
+console.log(url.split('.'))
+console.log(url.substr(7,2))
+console.log(url.substring(7,10))
+
+let s = 'magedu.edu'
+console.log(s.indexOf('ed'))
+console.log(s.indexOf('ed',4))
+console.log(s.replace('.edu','.com'))
+
+s = ' \tmag edu \r\n'
+console.log(s.trim())
