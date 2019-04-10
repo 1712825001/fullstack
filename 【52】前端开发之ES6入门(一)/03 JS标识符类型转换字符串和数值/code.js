60e905b7b2955d523b4ec27f6a4666dc73c52cf9
lining <1054057238@qq.com>
2019-04-10 14:14:44 +0800

    8341

--- 【52】前端开发之ES6入门(一)/03 JS标识符类型转换字符串和数值/code.js
+++ 【52】前端开发之ES6入门(一)/03 JS标识符类型转换字符串和数值/code.js
@@ -6,13 +6,46 @@
      * 多行注释
      * 
     */
-// 2、变量的定义
-// console.log(btoa)
 
-// a = 100 // 不推荐使用
-// var b = 200 // 当前作用域提升此声明
-// let c = 300 // ES6语法
+// 2、变量的定义
+/*
+console.log(b)
+
+a = 100 // 不推荐使用
+var b = 200 // 当前作用域提升此声明
+let c = 300 // ES6语法
+*/
 
 // 3. 常量
+/*
 const d = 400
 console.log(d)
+*/
+
+// 4、弱类型
+/*
+console.log('b' + 1)
+console.log(a = 3 + 'magedu', typeof(a))
+console.log(a = null + 'magedu', typeof(a))
+console.log(a = undefined + 'magedu', typeof(a))
+console.log(a = true + 'magedu', typeof(a))
+*/
+
+// 数字
+/*
+console.log(a = null + 8, typeof(a))
+console.log(a = undefined + 8, typeof(a))
+console.log(a = true + 8, typeof(a))
+console.log(a = false + 8, typeof(a))
+*/
+
+// boolean
+/*
+console.log(a = null + true, typeof(a))
+console.log(a = null + false, typeof(a))
+console.log(undefined + true, typeof(a))
+console.log(undefined + false, typeof(a))
+console.log(null & true, typeof(a))
+console.log(undefined & true, typeof(a))
+*/
+
